65404df3a124fee0ac3f539e616879f187a1e00b
Alexander Korobov <korobass1987@yandex.ru>
2022-01-12 19:39:04 +0300

    3-d

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,30 +3,24 @@ import throttle from "lodash.throttle";
 const mail = document.querySelector('.feedback-form input')
 const textArea = document.querySelector('.feedback-form textarea')
 const form = document.querySelector('.feedback-form')
-
+const parseValue = JSON.parse(localStorage.getItem("feedback-form-state"))
 let value = {}
 
 if (localStorage.getItem("feedback-form-state")) { 
-    value =  JSON.parse(localStorage.getItem("feedback-form-state"))
+    value =  parseValue
 }
-
-
-
-const parseValue = JSON.parse(localStorage.getItem("feedback-form-state"))
-
 
 if (localStorage.getItem("feedback-form-state")) {
     
-    if (parseValue.email){mail.value = parseValue.email}
-    if (parseValue.message){textArea.value = parseValue.message}
+    if (parseValue.email)
+    { mail.value = parseValue.email }
+    if (parseValue.message)
+    { textArea.value = parseValue.message }
     
 }
 
-
 form.addEventListener('submit', formSubmit)
 form.addEventListener('input', throttle(textInput, 500) )
-
-
 
 function formSubmit(event) {
     event.preventDefault()
